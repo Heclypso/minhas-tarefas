6cfeca98f6b82120643172b606a0ee37f808381f
Heclypso <heclypso@gmail.com>
2025-06-18 20:33:43 -0300

    Realizado as configurações iniciais

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,11 @@
+const ListaDeTarefas = () => {
+  return (
+    <main>
+      <p>
+        2 tarefas marcadas como: &quot;categoria&lquot; e &quot;termo&lquot;
+      </p>
+    </main>
+  )
+}
+
+export default ListaDeTarefas
