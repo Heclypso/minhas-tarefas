3a597d1b57f06987df8647bb520e5df84c14bf2d
Heclypso <heclypso@gmail.com>
2025-06-20 11:03:27 -0300

    Desenvolvido o card das tarefas

---
+++ src/containers/ListaDeTarefas/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  padding: 0px 40px;
+  height: 100vh;
+  overflow-y: scroll; // tudo que transbordar do container, pelo eixo Y, vai ser colocado dentro de um scroll
+`
