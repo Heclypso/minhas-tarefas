06874de5526c946909c41b175c671a603a01e645
Heclypso <heclypso@gmail.com>
2025-06-23 22:24:02 -0300

    Desenvolvido o estado de troca de botões e a renderização dinamica das tarefas

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,6 +1,27 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar TS',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Pegar a Kaguya',
+    descricao: 'Conseguir a kaguya antes do evento acabar',
+    prioridade: 'URGENTE',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Falhar em tentar pegar monarca',
+    descricao: 'Não conseguir pegar monarca hoje de novo',
+    prioridade: 'regular',
+    status: 'concluido'
+  }
+]
+
 const ListaDeTarefas = () => {
   return (
     <Container>
@@ -8,21 +29,16 @@ const ListaDeTarefas = () => {
         2 tarefas marcadas como: &quot;categoria&lquot; e &quot;termo&lquot;
       </p>
       <ul>
-        <li>
-          <Tarefa />
-        </li>
-        <li>
-          <Tarefa />
-        </li>
-        <li>
-          <Tarefa />
-        </li>
-        <li>
-          <Tarefa />
-        </li>
-        <li>
-          <Tarefa />
-        </li>
+        {tarefas.map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              titulo={t.titulo}
+              prioridade={t.prioridade}
+              status={t.status}
+              descricao={t.descricao}
+            />
+          </li>
+        ))}
       </ul>
     </Container>
   )
