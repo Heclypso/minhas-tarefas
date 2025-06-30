cc6016ee493e3b9387ee14b009b2a289f757a8ea
Heclypso <heclypso@gmail.com>
2025-06-30 19:17:44 -0300

    Desenvolvido sistema de edição de tarefa

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -1,16 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 
 import * as S from './styles'
 
-import { remover } from '../../store/reducers/tarefas'
+import { remover, editar } from '../../store/reducers/tarefas'
 import TarefaClass from '../../models/Tarefa'
 
 type Props = TarefaClass
 
-const Tarefa = ({ titulo, prioridade, status, descricao, id }: Props) => {
-  const [estaEditando, setEstaEditando] = useState(false)
+const Tarefa = ({
+  titulo,
+  prioridade,
+  status,
+  descricao: descricaoOriginal,
+  id
+}: Props) => {
   const dispatch = useDispatch()
+  const [estaEditando, setEstaEditando] = useState(false)
+  const [descricao, setDescricao] = useState('')
+
+  useEffect(() => {
+    if (descricaoOriginal.length > 0) {
+      setDescricao(descricaoOriginal)
+    }
+  }, [descricaoOriginal])
+
+  function cancelarEdição() {
+    setEstaEditando(false)
+    setDescricao(descricaoOriginal)
+  }
+
+  function editando() {
+    dispatch(
+      editar({
+        descricao,
+        prioridade,
+        status,
+        titulo,
+        id
+      })
+    )
+    setEstaEditando(false)
+  }
 
   return (
     <S.Card>
@@ -21,12 +52,16 @@ const Tarefa = ({ titulo, prioridade, status, descricao, id }: Props) => {
       <S.Tag parametro="status" status={status}>
         {status}
       </S.Tag>
-      <S.Descricao value={descricao} />
+      <S.Descricao
+        disabled={!estaEditando}
+        value={descricao}
+        onChange={(evento) => setDescricao(evento.target.value)}
+      />
       <S.BarraAcoes>
         {estaEditando ? (
           <>
-            <S.BotaoSalvar>Salvar</S.BotaoSalvar>
-            <S.BotaoCancelarRemover onClick={() => setEstaEditando(false)}>
+            <S.BotaoSalvar onClick={editando}>Salvar</S.BotaoSalvar>
+            <S.BotaoCancelarRemover onClick={cancelarEdição}>
               Cancelar
             </S.BotaoCancelarRemover>
           </>
